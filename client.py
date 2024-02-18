24552301534138f8580360ef75a327a63ca3d52f
kvan7 <kvan.valve@gmail.com>
2024-02-18 01:02:19 +0000

    update for remote

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from langserve import RemoteRunnable
 
-server = RemoteRunnable("http://localhost:8080/langchain")
+server = RemoteRunnable("https://ai.kvan.dev/langchain")
 
 result = server.invoke({"question": "What is a new AI tool released recently?"})
 print(result)
