c4dbd454c6ff16e4fbacd113fcdb78d040563b84
kvan7 <kvan.valve@gmail.com>
2024-02-18 04:35:36 +0000

    Updated history

--- server/app/LangChainRunnable.py
+++ server/app/LangChainRunnable.py
@@ -1,10 +1,10 @@
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Tuple
 from langchain_core.runnables import Runnable, RunnableConfig
 import os
 
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain import hub
-from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.messages import BaseMessage, AIMessage, HumanMessage, SystemMessage
 from langchain.agents import AgentExecutor, create_openai_functions_agent, create_openai_tools_agent
 from langchain.tools.retriever import create_retriever_tool
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -15,13 +15,17 @@ from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.language_models.chat_models import BaseChatModel
 from pydantic import Field
-
+from langchain_core.chat_history import BaseChatMessageHistory
 from app.CachingRSSFeedLoader import CachingRSSFeedLoader
-from langchain_core.messages import BaseMessage, AIMessage
 from langchain_core.outputs import ChatResult, ChatGeneration
-from langchain.prompts import ChatPromptTemplate
-from langchain.tools import tool
-from langchain_core.callbacks import Callbacks, CallbackManagerForLLMRun
+from langchain_core.callbacks import CallbackManagerForLLMRun
+
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
 
 class LangChainRunnable(BaseChatModel):
     langchain_key: str = Field(default_factory=lambda: os.getenv("LANGCHAIN_API_KEY"))
@@ -33,53 +37,15 @@ class LangChainRunnable(BaseChatModel):
     message_history: Optional[ChatMessageHistory] = None
 
     def __init__(self, **data):
-        super().__init__(**data)  # Pass the data to the parent class
+        super().__init__(**data)
         self.setup()
 
     def setup(self):
-        search = TavilySearchResults()
-        # search.invoke("what is the weather in Lincoln")
-
         with open("./app/rss_urls.txt") as f:
             urls = f.readlines()
         
-        loader = CachingRSSFeedLoader(cache_dir="./app/.cache", urls=urls, show_progress_bar=True, browser_user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
-        docs = loader.load()
-        filtered_docs = filter_complex_metadata(docs)
-        documents = RecursiveCharacterTextSplitter(
-            chunk_size=1000, chunk_overlap=200
-        ).split_documents(filtered_docs)
-        
-        vector = Chroma.from_documents(documents, OpenAIEmbeddings())
-        retriever = vector.as_retriever()
+        self.agent_with_chat_history= self.create_agent(urls)
 
-        # print(retriever.get_relevant_documents("api reference")[0])
-        retriever_tool = create_retriever_tool(
-            retriever,
-            "langchain_search",
-            "Search for information about LangChain. For any questions about LangChain, you must use this tool!"
-        )
-
-        tools = [search, retriever_tool]
-
-        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, streaming=True)
-        prompt = hub.pull("hwchase17/openai-functions-agent")
-
-        agent = create_openai_functions_agent(llm, tools, prompt)
-        self.agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-        # result = agent_executor.invoke({"input": "How do I create an agent with LangChain"} "chat_history": [])
-        # print(result)
-
-        self.message_history = ChatMessageHistory()
-
-        self.agent_with_chat_history = RunnableWithMessageHistory(
-            self.agent_executor,
-            # This is needed because in most real world scenarios, a session id is needed
-            # It isn't really used here because we are using a simple in memory ChatMessageHistory
-            lambda session_id: self.message_history,
-            input_messages_key="input",
-            history_messages_key="chat_history",
-        )
 
     @property
     def input_schema(self, config: Optional[RunnableConfig]) -> dict:
@@ -98,11 +64,66 @@ class LangChainRunnable(BaseChatModel):
                 "description": "The answer to the given question"
             }
         }
+    
+    def create_agent(self, urls: List[str], opml: str = None) -> RunnableWithMessageHistory:
+        search = TavilySearchResults()
+
+        loader = CachingRSSFeedLoader(cache_dir="./app/.cache", urls=urls, opml=opml, show_progress_bar=True, browser_user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+        docs = loader.load()
+        filtered_docs = filter_complex_metadata(docs)
+        documents = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        ).split_documents(filtered_docs)
+        
+        vector = Chroma.from_documents(documents, OpenAIEmbeddings())
+        retriever = vector.as_retriever()
+
+        retriever_tool = create_retriever_tool(
+            retriever,
+            "rss_retriever",
+            "Scour different rss feeds for information",
+        )
+
+        tools = [ retriever_tool]
+
+        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, streaming=True)
+        prompt = hub.pull("hwchase17/openai-functions-agent")
+
+        agent = create_openai_functions_agent(llm, tools, prompt)
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+        agent_with_chat_history = RunnableWithMessageHistory(
+            agent_executor,
+            get_session_history,
+            input_messages_key="input",
+            history_messages_key="chat_history",
+        )
+        return agent_with_chat_history
+    
+    def new_rss_system(self, messages: List[BaseMessage], is_opml: bool = False) -> RunnableWithMessageHistory:
+        if is_opml:
+            rss_feeds = messages[0].content
+            agent = self.create_agent(None, opml=messages[0].content)
+        else:
+            rss_feeds = messages[0].content.splitlines()[1:]
+            agent = self.create_agent(rss_feeds)
+        return agent
 
     def process_question(self, messages: List[BaseMessage], config: Optional[RunnableConfig] = {"configurable":{"session_id":"<foo>"}}) -> str:
-        self.message_history.clear()
-        self.message_history.add_messages(messages[:-1])
-        output = self.agent_with_chat_history.invoke(
+        agent = self.agent_with_chat_history
+        len_req = 1
+        if isinstance(messages[0], SystemMessage):
+            if "rss" in messages[0].content.splitlines()[0].lower():
+                agent = self.new_rss_system(messages)
+            elif "<?xml version='1.0' encoding='UTF-8' ?>" in messages[0].content.splitlines()[0] \
+                    and "<opml version=" in messages[0].content.splitlines()[1]:
+                agent = self.new_rss_system(messages, is_opml=True)
+            len_req = 2
+        if len(messages) > len_req:
+            history = get_session_history(config["configurable"]["session_id"])
+            history.add_messages(messages[:-1] if len_req == 1 else messages[1:-1])
+            print("History: ", history.messages)
+        output = agent.invoke(
             {"input": messages[-1].content},
             config=config
         )
@@ -115,7 +136,7 @@ class LangChainRunnable(BaseChatModel):
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
     ) -> ChatResult:
-        response_content = self.process_question(messages)
+        response_content = self.process_question(messages, config={"configurable":{"session_id":run_manager.run_id}})
         response_message = AIMessage(content=response_content)
         return ChatResult(generations=[ChatGeneration(message=response_message)])
 
