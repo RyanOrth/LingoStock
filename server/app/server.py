8702bb6b0f9578215fe12d529b1e5acb92d10424
RyanOrth <ryan.orth@live.com>
2024-02-18 08:07:32 +0000

    Got basic agi runnable working without history or streaming

--- server/app/server.py
+++ server/app/server.py
@@ -4,6 +4,7 @@ from langserve import add_routes
 from dotenv import load_dotenv
 
 from app.QueryRunnable import QueryRunnable
+from app.AgiRunnable import AgiRunnable
 
 load_dotenv()
 
@@ -15,8 +16,11 @@ async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
 
-langchain_runnable = QueryRunnable()
-add_routes(app, langchain_runnable, path="/query")
+query_runnable = QueryRunnable()
+add_routes(app, query_runnable, path="/query")
+
+agi_runnable = AgiRunnable()
+add_routes(app, agi_runnable, path="/agi")
 
 if __name__ == "__main__":
     import uvicorn
